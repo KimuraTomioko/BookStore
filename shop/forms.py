f4cf1aa865bfc78d5fe08ed9fffb4e3e835886d5
KIMURA TOMIOKO <122150562+KimuraTomioko@users.noreply.github.com>
2024-07-04 15:22:53 +0300

    initial commit

--- shop/forms.py
+++ shop/forms.py
@@ -1,5 +1,7 @@
 from django import forms
+import re
 from .models import *
+from django.core.exceptions import ValidationError
 
 class ProductCreationForm(forms.ModelForm):
     class Meta:
@@ -8,8 +10,6 @@ class ProductCreationForm(forms.ModelForm):
             'name',
             'description',
             'price',
-            'create_date',
-            'update_date',
             'photo',
             'is_exists',
             'parametr',
@@ -18,18 +18,15 @@ class ProductCreationForm(forms.ModelForm):
         )
 
         widgets = {
-            'name': forms.TextInput(attrs = {'class':'form-control'}),
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
             'description': forms.Textarea(attrs={'class': 'form-control'}),
-            'price': forms.FloatField(attrs={'class':'form-control'}),
-            'create_date': forms.DateTimeField(attrs={'class': 'form-control'}),
-            'update_date': forms.DateTimeField(attrs={'class': 'form-control'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control'}),
             'photo': forms.ClearableFileInput(attrs={'class': 'form-control'}),
             'is_exists': forms.CheckboxInput(attrs={'class': 'form-control'}),
             'parametr': forms.SelectMultiple(attrs={'class': 'form-control'}),
             'category': forms.Select(attrs={'class': 'form-control'}),
-            'tag': forms.SelectMultiple(attrs={'class':'form-control'})
+            'tag': forms.SelectMultiple(attrs={'class': 'form-control'}),
         }
-    
 
     parametr = forms.ModelMultipleChoiceField(
         queryset=Parametr.objects.all(),
@@ -38,10 +35,44 @@ class ProductCreationForm(forms.ModelForm):
 
     category = forms.ModelChoiceField(
         queryset=Category.objects.all(),
-        widget=forms.Select(attrs={'class':'form-control'})
+        widget=forms.Select(attrs={'class': 'form-control'})
     )
 
     tag = forms.ModelMultipleChoiceField(
         queryset=Tag.objects.all(),
         widget=forms.SelectMultiple(attrs={'class': 'form-control'})
     )
+
+
+class ProductFilterForm(forms.Form):
+    name = forms.CharField(
+        label='Название товара',
+        required=False,
+        min_length=2,
+        max_length=150,
+        strip=True,
+        widget=forms.TextInput(
+            attrs={'class': 'form-control'}
+        )
+    )
+    min_price = forms.DecimalField(
+        label='Минимальная цена',
+        required=False,
+        min_value=0,
+        decimal_places=2,
+        max_digits=10,
+        widget=forms.NumberInput(
+            attrs={'class': 'form-control'}
+        )
+    )
+    max_price = forms.DecimalField(
+        label='Максимальная цена',
+        required=False,
+        min_value=1,
+        decimal_places=2,
+        max_digits=10,
+        widget=forms.NumberInput(
+            attrs={'class': 'form-control'}
+        )
+
+   )
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, render
 from .models import *
 from .forms import * 
-
+from django.contrib.auth.decorators import login_required, permission_required
 
 def product_list(request):
     list_product = Product.objects.all()
@@ -17,26 +17,58 @@ def get_one_filter_product(request):
         'find_product': find_product
     }
 
-    return render(request, 'shop/product/filter.html')
+    return render(request, 'shop/product/filter.html', context)
+
+
+def get_more_filter_product(request):
+    find_product = Product.objects.filter(
+        price__lte=request.GET.get('max_price'),
+        price__gt=request.GET.get('min_price')
+    )
+    context = {
+        'find_product': find_product
+    }
+    return render(request, 'shop/product/query_filter_product.html', context)
+
+
+def product_catalog_with_filter(request):
+    list_product = Product.objects.all()
+    if request.GET:
+        product_form = ProductFilterForm(request.GET)
+    else:
+        product_form = ProductFilterForm()
+
+    if product_form.is_valid():
+        if product_form.cleaned_data.get('name'):
+            list_product = list_product.filter(name__icontains=product_form.cleaned_data.get('name'))
+        if product_form.cleaned_data.get('min_price'):
+            list_product = list_product.filter(price__gte=product_form.cleaned_data.get('min_price'))
+        if product_form.cleaned_data.get('max_price'):
+            list_product = list_product.filter(price__lte=product_form.cleaned_data.get('max_price'))
+        
+    context = {
+        'product_list': list_product,
+        'form': product_form
+    }
+    return render(request, 'shop/product/catalog_with_filter.html', context)
 
 
 
-
-
-
+@login_required
 def create_product(request):
     if request.method == 'POST':
         product_create_form = ProductCreationForm(request.POST)
         if product_create_form.is_valid():
-            new_product_create_form = ProductCreationForm(**product_create_form.cleaned_data)
-            new_product_create_form.save()
-            return redirect('catalog')
+            product_create_form.save()
+            return redirect('product_list')
         else:
             return render(request, 'shop/product/create_product.html', {'form': product_create_form})
-    
     else:
-        product_creation_form = ProductCreationForm()
+        product_create_form = ProductCreationForm()
         return render(request, 'shop/product/create_product.html', {'form': product_create_form})
+
+
+
     
 
     
