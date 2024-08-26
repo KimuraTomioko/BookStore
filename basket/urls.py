02e9f4fb1147b408121658d9615fb00f26c08895
KIMURA TOMIOKO <122150562+KimuraTomioko@users.noreply.github.com>
2024-08-26 16:53:52 +0300

    initial commit

--- basket/urls.py
+++ basket/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from .views import *
+
+
 urlpatterns = [
-    
+    path('', basket_detail, name='basket_detail'),
+    path('add/<int:product_id>/', basket_add, name='basket_add')
 ]
--- basket/views.py
+++ basket/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+from shop.models import Product, Order, Pos_order
+from .forms import BasketAddProductForm
+from .basket import Basket
 
-# Create your views here.
+def basket_detail(request):
+    basket = Basket(request)
+    context = {
+        'basket': basket
+    }
+    return render(request, 'basket/detail.html')
+
+@require_POST
+def basket_add(request, product_id):
+    basket = Basket(request)
+    product = get_object_or_404(Product, pk=product_id)
+    form = BasketAddProductForm(request.POST)
+    if form.is_valid():
+        basket.add(
+            product=product,
+            count=form.cleaned_data['count'],
+            update_count=form.cleaned_data['update']
+        )
+    return redirect('basket_detail')
+    
